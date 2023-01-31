1f42743237f74ff8827f9d3fffbf32720581927a
piomin <piotr.minkowski@gmail.com>
2023-01-31 23:52:33 +0100

    adding testcontainers

--- producer/src/test/java/pl/piomin/samples/amqp/producer/ProducerApplicationTests.java
+++ producer/src/test/java/pl/piomin/samples/amqp/producer/ProducerApplicationTests.java
@@ -1,13 +1,33 @@
 package pl.piomin.samples.amqp.producer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class ProducerApplicationTests {
+
+    @Autowired
+    ProducerService service;
+
+    @Container
+    static final RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:latest")
+            .withExposedPorts(5672);
+
+    @DynamicPropertySource
+    static void rabbitProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.rabbitmq.host", rabbitmq::getHost);
+        registry.add("spring.rabbitmq.port", rabbitmq::getAmqpPort);
+    }
 
     @Test
     void startup() {
-
+        service.send("trx-events-topic");
     }
 }
