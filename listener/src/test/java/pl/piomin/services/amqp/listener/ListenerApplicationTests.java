ba7dec108fbd4c2ac0c006156685beae43a6b2e4
piomin <piotr.minkowski@gmail.com>
2023-01-31 16:14:23 +0100

    enable auto-update + adding empty test

---
+++ listener/src/test/java/pl/piomin/services/amqp/listener/ListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piomin.services.amqp.listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ListenerApplicationTests {
+
+    @Test
+    void startup() {
+
+    }
+}
---
+++ producer/src/test/java/pl/piomin/samples/amqp/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piomin.samples.amqp.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProducerApplicationTests {
+
+    @Test
+    void startup() {
+
+    }
+}
